4e377437a877b4ec9456aa56b38d9221480412f9
m1ga <migamiga@web.de>
2021-12-01 21:42:29 +0100

    android 12 permissions

--- android/src/ti/thermalprinter/TiThermalprinterModule.java
+++ android/src/ti/thermalprinter/TiThermalprinterModule.java
@@ -1,6 +1,13 @@
 package ti.thermalprinter;
 
+import android.Manifest;
+import android.app.Activity;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+
 import com.dantsu.escposprinter.EscPosPrinter;
+import com.dantsu.escposprinter.connection.bluetooth.BluetoothConnection;
 import com.dantsu.escposprinter.connection.bluetooth.BluetoothPrintersConnections;
 import com.dantsu.escposprinter.connection.tcp.TcpConnection;
 import com.dantsu.escposprinter.exceptions.EscPosBarcodeException;
@@ -11,19 +18,29 @@ import com.dantsu.escposprinter.exceptions.EscPosParserException;
 import org.appcelerator.kroll.KrollDict;
 import org.appcelerator.kroll.KrollModule;
 import org.appcelerator.kroll.annotations.Kroll;
+import org.appcelerator.kroll.common.Log;
 import org.appcelerator.kroll.common.TiConfig;
 import org.appcelerator.titanium.TiApplication;
+import org.appcelerator.titanium.TiBaseActivity;
+import org.appcelerator.titanium.TiC;
 import org.appcelerator.titanium.util.TiConvert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pub.devrel.easypermissions.EasyPermissions;
 
 
 @Kroll.module(name = "TiThermalprinter", id = "ti.thermalprinter")
-public class TiThermalprinterModule extends KrollModule {
+public class TiThermalprinterModule extends KrollModule implements EasyPermissions.PermissionCallbacks,
+        EasyPermissions.RationaleCallbacks {
 
     // You can define constants with @Kroll.constant, for example:
     @Kroll.constant
     public static final int TYPE_BLUETOOTH = 0;
     @Kroll.constant
     public static final int TYPE_TCP = 1;
+    public static final String[] BLUETOOTH_PERMISSIONS_S = {Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.BLUETOOTH_CONNECT};
     // Standard Debugging variables
     private static final String LCAT = "TiThermalprinterModule";
     private static final boolean DBG = TiConfig.LOGD;
@@ -37,7 +54,41 @@ public class TiThermalprinterModule extends KrollModule {
 
     }
 
+
     // Methods
+    @Kroll.method
+    public void scanPrinters() {
+        BluetoothPrintersConnections bluetoothPrintersConnections = new BluetoothPrintersConnections();
+        BluetoothConnection[] bluetoothPrinters = bluetoothPrintersConnections.getList();
+        for (BluetoothConnection printer : bluetoothPrinters) {
+            Log.i("printer", printer.getDevice().getName());
+            try {
+                printer.connect();
+            } catch (EscPosConnectionException e) {
+                Log.e("---", e.getMessage());
+            }
+        }
+    }
+
+    @Kroll.method
+    public void requestPermissions() {
+        Activity activity = TiApplication.getInstance().getCurrentActivity();
+
+        // Create the permission list.
+        ArrayList<String> permissionList = new ArrayList<>(4);
+        permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        if (Build.VERSION.SDK_INT >= 31) {
+            permissionList.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+            permissionList.add(Manifest.permission.BLUETOOTH_ADVERTISE);
+            permissionList.add(Manifest.permission.BLUETOOTH_CONNECT);
+            permissionList.add(Manifest.permission.BLUETOOTH_SCAN);
+        }
+
+        // Show dialog requesting permission.
+        activity.requestPermissions(permissionList.toArray(new String[0]), TiC.PERMISSION_CODE_LOCATION);
+    }
+
+
     @Kroll.method
     public void print(KrollDict options) {
         String txt = TiConvert.toString(options.getString("text"), "");
@@ -85,6 +136,33 @@ public class TiThermalprinterModule extends KrollModule {
         }
     }
 
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+        Log.i("---", "Granted");
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+        Log.i("---", "result");
+    }
+
+    @Override
+    public void onRationaleAccepted(int requestCode) {
+
+    }
+
+    @Override
+    public void onRationaleDenied(int requestCode) {
+
+    }
+
 	/*
 	@Kroll.getProperty
 	public String getExampleProp()
@@ -100,4 +178,3 @@ public class TiThermalprinterModule extends KrollModule {
 	}
 	*/
 }
-
