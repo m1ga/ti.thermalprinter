9dbcf09afe606feb679e73559e29e42922bfe80f
m1ga <migamiga@web.de>
2022-07-20 22:00:58 +0200

    usb

--- android/src/ti/thermalprinter/TiThermalprinterModule.java
+++ android/src/ti/thermalprinter/TiThermalprinterModule.java
@@ -1,15 +1,26 @@
 package ti.thermalprinter;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
 import android.os.Build;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 
 import com.dantsu.escposprinter.EscPosPrinter;
 import com.dantsu.escposprinter.connection.bluetooth.BluetoothConnection;
 import com.dantsu.escposprinter.connection.bluetooth.BluetoothPrintersConnections;
 import com.dantsu.escposprinter.connection.tcp.TcpConnection;
+import com.dantsu.escposprinter.connection.usb.UsbConnection;
+import com.dantsu.escposprinter.connection.usb.UsbPrintersConnections;
 import com.dantsu.escposprinter.exceptions.EscPosBarcodeException;
 import com.dantsu.escposprinter.exceptions.EscPosConnectionException;
 import com.dantsu.escposprinter.exceptions.EscPosEncodingException;
@@ -40,22 +51,24 @@ public class TiThermalprinterModule extends KrollModule implements EasyPermissio
     public static final int TYPE_BLUETOOTH = 0;
     @Kroll.constant
     public static final int TYPE_TCP = 1;
+    @Kroll.constant
+    public static final int TYPE_USB = 2;
+
     public static final String[] BLUETOOTH_PERMISSIONS_S = {Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.BLUETOOTH_CONNECT};
     // Standard Debugging variables
     private static final String LCAT = "TiThermalprinterModule";
-    private static final boolean DBG = TiConfig.LOGD;
+    private String printText = "";
 
     public TiThermalprinterModule() {
         super();
     }
 
     @Kroll.onAppCreate
-    public static void onAppCreate(TiApplication app) {
-
-    }
+    public static void onAppCreate(TiApplication app) {}
 
 
     // Methods
+    @SuppressLint("MissingPermission")
     @Kroll.method
     public void scanPrinters() {
         BluetoothPrintersConnections bluetoothPrintersConnections = new BluetoothPrintersConnections();
@@ -70,6 +83,7 @@ public class TiThermalprinterModule extends KrollModule implements EasyPermissio
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Kroll.method
     public void requestPermissions() {
         Activity activity = TiApplication.getInstance().getCurrentActivity();
@@ -88,18 +102,42 @@ public class TiThermalprinterModule extends KrollModule implements EasyPermissio
         activity.requestPermissions(permissionList.toArray(new String[0]), TiC.PERMISSION_CODE_LOCATION);
     }
 
+    private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
+    private final BroadcastReceiver usbReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (ACTION_USB_PERMISSION.equals(action)) {
+                synchronized (this) {
+                    UsbManager usbManager = (UsbManager) TiApplication.getAppCurrentActivity().getSystemService(Context.USB_SERVICE);
+                    UsbDevice usbDevice = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        if (usbManager != null && usbDevice != null) {
+                            try {
+                                EscPosPrinter printer = new EscPosPrinter(new UsbConnection(usbManager, usbDevice), 203, 48f, 32);
+                                printer.printFormattedText(printText);
+                            } catch (EscPosConnectionException | EscPosBarcodeException | EscPosEncodingException | EscPosParserException e) {
+                                KrollDict kd = new KrollDict();
+                                kd.put("message", e.getMessage());
+                                fireEvent("error", kd);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    };
 
     @Kroll.method
     public void print(KrollDict options) {
-        String txt = TiConvert.toString(options.getString("text"), "");
+        printText = TiConvert.toString(options.getString("text"), "");
         int printType = TiConvert.toInt(options.get("connection"), 0);
 
 
-        if (txt != "") {
+        if (printText != "") {
             if (printType == TYPE_BLUETOOTH) {
                 try {
                     EscPosPrinter printer = new EscPosPrinter(BluetoothPrintersConnections.selectFirstPaired(), 203, 48f, 32);
-                    printer.printFormattedText(txt);
+                    printer.printFormattedText(printText);
                 } catch (EscPosConnectionException | EscPosParserException | EscPosEncodingException | EscPosBarcodeException e) {
                     KrollDict kd = new KrollDict();
                     kd.put("message", e.getMessage());
@@ -117,7 +155,7 @@ public class TiThermalprinterModule extends KrollModule implements EasyPermissio
                 if (ip != "" && port != 0) {
                     try {
                         EscPosPrinter printer = new EscPosPrinter(new TcpConnection(ip, port, timeout), dpi, width, cpl);
-                        printer.printFormattedText(txt);
+                        printer.printFormattedText(printText);
                     } catch (EscPosConnectionException | EscPosBarcodeException | EscPosEncodingException | EscPosParserException e) {
                         KrollDict kd = new KrollDict();
                         kd.put("message", e.getMessage());
@@ -127,6 +165,20 @@ public class TiThermalprinterModule extends KrollModule implements EasyPermissio
                     KrollDict kd = new KrollDict();
                     kd.put("message", "Check IP and port");
                     fireEvent("error", kd);
+                }
+            } else if (printType == TYPE_USB) {
+                UsbConnection usbConnection = UsbPrintersConnections.selectFirstConnected(TiApplication.getAppCurrentActivity());
+                UsbManager usbManager = (UsbManager)  TiApplication.getAppCurrentActivity().getSystemService(Context.USB_SERVICE);
+                if (usbConnection != null && usbManager != null) {
+                    PendingIntent permissionIntent = PendingIntent.getBroadcast(
+                            TiApplication.getAppCurrentActivity(),
+                            0,
+                            new Intent(ACTION_USB_PERMISSION),
+                            android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S ? PendingIntent.FLAG_MUTABLE : 0
+                    );
+                    IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+                    TiApplication.getAppCurrentActivity().registerReceiver(this.usbReceiver, filter);
+                    usbManager.requestPermission(usbConnection.getDevice(), permissionIntent);
                 }
             }
         } else {
@@ -162,19 +214,4 @@ public class TiThermalprinterModule extends KrollModule implements EasyPermissio
     public void onRationaleDenied(int requestCode) {
 
     }
-
-	/*
-	@Kroll.getProperty
-	public String getExampleProp()
-	{
-		Log.d(LCAT, "get example property");
-		return "hello world";
-	}
-
-
-	@Kroll.setProperty
-	public void setExampleProp(String value) {
-		Log.d(LCAT, "set example property: " + value);
-	}
-	*/
 }
